ee196077b64bf436f876134944834e0e4432c6ef
Bibi <bibi6666667@kakao.com>
2022-12-05 23:09:39 +0900

    11강 dispose and terminating

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -1,22 +1,21 @@
 import UIKit
 import RxSwift
 
-let observable = Observable.just(1)
+let disposeBag = DisposeBag()
 
-let observable2 = Observable.of(1, 2, 3)
-let observable3 = Observable.of([1, 2, 3])
-let observable4 = Observable.from([1, 2, 3, 4, 5])
+Observable.of("A", "B", "C")
+    .subscribe{
+        print($0)
+    }.disposed(by: disposeBag)
 
-observable4.subscribe { event in
-    if let element = event.element {
-        print(element)
-    }
-}
 
-observable3.subscribe { event in
-    if let element = event.element {
-        print(element)
-    }
-}
-
- 
+Observable<String>.create { observer in
+    observer.onNext("A")
+    observer.onCompleted()
+    observer.onNext("?")
+    return Disposables.create()
+}.subscribe(onNext: { print($0) },
+            onError: { print($0) },
+            onCompleted: { print("Completed") },
+            onDisposed: { print("Disposed")})
+.disposed(by: disposeBag)
