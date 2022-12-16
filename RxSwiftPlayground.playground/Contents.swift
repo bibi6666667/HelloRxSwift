105c39111f9b3e8efbc05631c2e778e5b77c99eb
Bibi <bibi6666667@kakao.com>
2022-12-16 22:43:53 +0900

    skip, skipWhile, skipUntil, take, takeWhile, takeUntil

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -6,11 +6,56 @@ let strikes = PublishSubject<String>()
 
 let disposeBag = DisposeBag()
 
-strikes.elementAt(2)
-    .subscribe(onNext: { str in
-        print("\(str) - You are out!")
+Observable.of("A", "B", "C", "D", "E", "F")
+    .skip(4)
+    .subscribe(onNext: {
+        print($0)
     }).disposed(by: disposeBag)
 
-strikes.onNext("1X")
-strikes.onNext("2X")
-strikes.onNext("3X")
+Observable.of(2, 2, 3, 4, 4)
+    .skipWhile { $0 % 2 == 0 }
+    .subscribe(onNext: {
+        print($0)
+    }).disposed(by: disposeBag)
+
+let subject = PublishSubject<String>()
+let trigger = PublishSubject<String>()
+
+subject.takeUntil(trigger)
+    .subscribe(onNext: {
+        print($0)
+    }).disposed(by: disposeBag)
+
+subject.onNext("11")
+subject.onNext("22")
+
+trigger.onNext("XX")
+
+subject.onNext("33")
+
+subject.skipUntil(trigger)
+    .subscribe(onNext: {
+        print($0)
+    }).disposed(by: disposeBag)
+
+subject.onNext("A")
+subject.onNext("B")
+
+trigger.onNext("X")
+
+subject.onNext("C")
+
+Observable.of(1, 2, 3, 4, 5, 6)
+    .take(3)
+    .subscribe(onNext: {
+        print($0)
+    }).disposed(by: disposeBag)
+
+Observable.of(2, 4, 6, 7, 8, 10)
+    .takeWhile {
+        return $0 % 2 == 0
+    }
+    .subscribe(onNext: {
+        print($0)
+    }).disposed(by: disposeBag)
+
