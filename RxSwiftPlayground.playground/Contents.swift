c2a1d486def7f64442ba582d0b731b942195385f
Bibi <bibi6666667@kakao.com>
2022-12-25 11:50:48 +0900

    49. flatMap

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -4,10 +4,25 @@ import RxCocoa
 
 let disposeBag = DisposeBag()
 
-Observable.of(1, 2, 3, 4, 5)
-    .map {
-        $0 * 2
-    }
+struct Student {
+    var score: BehaviorRelay<Int>
+}
+
+let john = Student(score: BehaviorRelay(value: 75))
+let mary = Student(score: BehaviorRelay(value: 95))
+
+let student = PublishSubject<Student>()
+
+student.asObservable()
+    .flatMap { $0.score.asObservable() }
     .subscribe(onNext: {
         print($0)
     }).disposed(by: disposeBag)
+
+student.onNext(john) // 75
+john.score.accept(100) // 100
+
+student.onNext(mary) // 95
+mary.score.accept(80) // 80
+
+john.score.accept(43) // 43
