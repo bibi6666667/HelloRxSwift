933ebe1e2f5717c715c36fc56c8c3dcf369bdfc2
Bibi <bibi6666667@kakao.com>
2022-12-25 11:40:49 +0900

    48. map

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -5,7 +5,9 @@ import RxCocoa
 let disposeBag = DisposeBag()
 
 Observable.of(1, 2, 3, 4, 5)
-    .toArray()
+    .map {
+        $0 * 2
+    }
     .subscribe(onNext: {
         print($0)
     }).disposed(by: disposeBag)
