34391e70a57a4e5b1dd42fca492d7cc8acac5ab5
Bibi <bibi6666667@kakao.com>
2022-12-27 17:41:14 +0900

    63. withLatestFrom

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -3,29 +3,17 @@ import RxSwift
 
 let disposeBag = DisposeBag()
 
-let left = PublishSubject<Int>()
-let middle = PublishSubject<Int>()
-let right = PublishSubject<String>()
+let button = PublishSubject<Void>()
+let textField = PublishSubject<String>()
 
-let observable = Observable.combineLatest(left, middle, right, resultSelector: {
-    lastLeft, lastMiddle, lastRight in
-    return "\(lastLeft) \(lastMiddle) \(lastRight)"
+let observable = button.withLatestFrom(textField)
+observable.subscribe(onNext: {
+    print($0)
 })
 
-observable.subscribe(onNext: { result in
-    print(result)
-}).disposed(by: disposeBag)
+textField.onNext("Sw")
+textField.onNext("Swif")
+textField.onNext("Swift")
 
-left.onNext(1)
-middle.onNext(10)
-right.onNext("A")
-left.onNext(2)
-middle.onNext(9)
-right.onNext("B")
-left.onNext(3)
-middle.onNext(8)
-right.onNext("C")
-left.onNext(4)
-middle.onNext(7)
-right.onNext("D")
-left.onNext(5)
+button.onNext(()) // 빈 클로저 전달
+button.onNext(())
